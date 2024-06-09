eea949daab99e3173aaa00e3593892aa823d9fe4
hirajya <rodneyestrada2425@gmail.com>
2024-06-09 21:01:16 +0800

    goods for mark merge

--- src/App.java
+++ src/App.java
@@ -1,5 +1,36 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+
+public class App extends Application{
+
+    public static void main(String[] args) {
+        launch(args);
     }
+
+    
+    public void start(Stage stage) throws Exception {
+        
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/view/main.fxml"));
+            Scene scene = new Scene(root);
+
+            // Image icon = new Image("vectorImages/movieHubLogo.png");
+            // stage.getIcons().add(icon);
+            stage.setTitle("Sonigirapp");
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
