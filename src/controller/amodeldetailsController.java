4160c6b3f3654b5cf2a6474080f782c36a292476
Mark Rhey Anthony De Luna <115197832+RheyMartt@users.noreply.github.com>
2024-06-23 22:34:14 +0800

    FXML connections
    model & unit details
    add unit

---
+++ src/controller/amodeldetailsController.java
@@ -0,0 +1,60 @@
+package controller;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class amodeldetailsController {
+
+    @FXML
+    Button dashboardButton, reportsButton, addunitButton, editmodelButton, backButton;
+
+    @FXML
+    TextField search_tf;
+
+    @FXML
+    public void initialize() {
+        // Initialization code if needed
+    }
+
+    public void goToDashboard(ActionEvent event) throws IOException {
+        System.out.println("Dashboard button clicked");
+        changeScene(event, "/view/adashboardscansandreports.fxml");
+    }
+
+    public void goToReports(ActionEvent event) throws IOException {
+        System.out.println("Reports button clicked");
+        changeScene(event, "/view/areports.fxml");
+    }
+
+    public void addunit(ActionEvent event) throws IOException {
+        System.out.println("Add Unit button clicked");
+        changeScene(event, "/view/ainventoryaddunit.fxml");
+    }
+
+    public void editmodel(ActionEvent event) throws IOException {
+        System.out.println("Edit Model button clicked");
+        changeScene(event, "/view/ainventoryeditmodel.fxml");
+    }
+
+    public void backToInventory(ActionEvent event) throws IOException {
+        System.out.println("Back button clicked");
+        changeScene(event, "/view/ainventory.fxml");
+    }
+
+    public void changeScene(ActionEvent event, String fxml) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource(fxml));
+        Scene scene = new Scene(root);
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+}
